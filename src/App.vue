8551ba1e8608060814aa6c27d7478ec984e335cb
Omkar Kasar <omkarkasar101010@gmail.com>
2024-12-04 20:51:33 +0530

    First

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+  <template>
+    <div>
+      <Header />
+    </div>
+  </template>
+
+<script>
+import Header from './components/Header.vue'; // Adjust the path if necessary
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="!useris">
+    <button  @click="signIn">Sign In</button>
+  </div>
+  <div v-else>
+    <button  @click="signOut" >Sign Out</button>
+    <button>Profile</button>
+
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      useris: false
+    }
+  },
+  methods:{
+      signIn()
+      {
+        this.useris=true;
+      },
+      signOut()
+      {
+        this.useris=false;
+      }
+  }
+}
+
+</script>
